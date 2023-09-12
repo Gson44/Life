3ea35f1dd68e91ec7aa06f6311c00035b93bd8e6
Gerson <geric99997@gmail.com>
2023-09-12 15:35:56 -0600

    Add responsive

--- app.py
+++ app.py
@@ -11,11 +11,18 @@ from sklearn.metrics import accuracy_score
 app = Flask(__name__)
 
 # Use Railway's environment variables for MySQL
+
 app.config["MYSQL_HOST"] = os.environ.get("MYSQLHOST")
 app.config["MYSQL_USER"] = os.environ.get("MYSQLUSER")
 app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQLPASSWORD")
 app.config["MYSQL_DB"] = os.environ.get("MYSQLDATABASE")
 app.config["MYSQL_PORT"] = int(os.environ.get("MYSQLPORT", 3306))  # default to 3306 if not provided
+"""
+app.config["MYSQL_HOST"] = 'localhost'
+app.config["MYSQL_USER"] = 'root'
+
+app.config["MYSQL_DB"] = 'life'
+"""
 
 app.config['SECRET_KEY'] = os.urandom(24)
 # Create mysql obkect
@@ -39,13 +46,13 @@ def sign_in_data():
             cursor.execute("SELECT id FROM user WHERE username = %s AND password = %s", (name, password))
             user = cursor.fetchone()
             cursor.close()
-            print(user)
+            #print(user)
             if user: 
-                print("In sign_in_data:",user[0])
+               # print("In sign_in_data:",user[0])
                 session["user_id"] = user[0]
                 return redirect(url_for("dashboard"))
             else:
-                print("Invalid")
+                #print("Invalid")
                 return "Failed"
         except Exception as e:
             print("Error", str(e))
@@ -56,7 +63,7 @@ def sign_in_data():
 def sign_up_page():
     return render_template("sign_up.html")
 
-# Sending data to sign up 
+# Sending data to sign up
 @app.route("/sign_up_data", methods=["POST"])
 def sign_up_data():
     if request.method == "POST":
@@ -64,19 +71,15 @@ def sign_up_data():
         username = request.form["username"]
         password = request.form["password"]
 
-        cursor = mysql.connection.cursor()
-        
         try:
-            cursor.execute("INSERT INTO user(name, username, password) VALUES(%s, %s, %s)", (name,username,password))
-            mysql.connection.commit()
+            with mysql.connection.cursor() as cursor:
+                cursor.execute("INSERT INTO user (name, username, password) VALUES (%s, %s, %s)", (name, username, password))
+                mysql.connection.commit()
+            return redirect(url_for("home"))
         except Exception as e:
-            print(str(e))
-
-        cursor.close()
-        mysql.connection.close()
-        
-        return redirect(url_for("home"))
-    return "Sign up failure"
+            #print(str(e))
+            return "Sign up failure"
+    return "Invalid request method"
 
 def convert_date_to_str(row):
     row_list = list(row)
@@ -99,22 +102,23 @@ def dashboard():
         habit_data = cursor.fetchall()
         session["habit_data"] = habit_data
         cursor.close()
-        print(habit_data)
+        #print(habit_data)
         if user_data:
-            print("Data grab successfully and transferred over")
+            #print("Data grab successfully and transferred over")
             
             if user_data[4] == 0:
                 return render_template('add_habit.html', user_data=user_data[1], habit_data=habit_data, status=user_data[4])
             else:
                 cleaned_habit_data = [convert_date_to_str(row) for row in habit_data]
                 habit_data_json = json.dumps(cleaned_habit_data)
+                
                 predict_habit = machine_learning_integration(user_id)
                 result = display_result(predict_habit)
-                return render_template('dashboard.html', user_data=user_data[1], habit_data = habit_data, data=habit_data_json, status=user_data[4], predict=result)
+                #print(result)
+                return render_template('dashboard.html', user_data=user_data[1], habit_data = habit_data, data=habit_data_json, status=user_data[4],predicti=result)
     return redirect(url_for("home"))
 
 
-# Create habit
 @app.route("/create_habit", methods=["POST"])
 def create_habit():
     if request.method == "POST":
@@ -122,25 +126,23 @@ def create_habit():
         habit2 = request.form["habit2"]
         habit3 = request.form["habit3"]
         user_id = session.get("user_id")
-        print(habit1)
-        print(habit2)
-        print(habit3)
-        print(user_id)
         today = date.today()
+        
         try:
-            print("Begin sql")
-            cursor = mysql.connection.cursor()
-            cursor.execute("INSERT INTO habits (habit_1_name, habit_2_name, habit_3_name, id, habit_date) VALUES (%s, %s, %s, %s, %s) ", (habit1, habit2,habit3,user_id,today))
-            print("data inserted for habits")
-            mysql.connection.commit()
-            cursor.execute("UPDATE user SET new_status = 1 WHERE id = %s", (user_id,))
-            print("data inserted for users")
-            mysql.connection.commit()
+            with mysql.connection.cursor() as cursor:
+                cursor.execute("INSERT INTO habits (habit_1_name, habit_2_name, habit_3_name, id, habit_date) VALUES (%s, %s, %s, %s, %s)", (habit1, habit2, habit3, user_id, today))
+                mysql.connection.commit()
+                cursor.execute("UPDATE user SET new_status = 1 WHERE id = %s", (user_id,))
+                mysql.connection.commit()
             return redirect(url_for("dashboard"))
         except Exception as e:
             print(str(e))
             mysql.connection.rollback()
-        cursor.close()
+        
+        return "Failed to create habit"
+
+    return "Invalid request method"
+
 
 @app.route("/send_habit", methods=["GET", "POST"])
 def send_habit():
@@ -154,7 +156,7 @@ def send_habit():
     habit_data = session["habit_data"]
 
     # Check for values and add T or F
-    habit1 = True if habit1 == "checked" else False 
+    habit1 = True if habit1 == "checked" else False
     habit2 = True if habit2 == "checked" else False
     habit3 = True if habit3 == "checked" else False
 
@@ -162,25 +164,23 @@ def send_habit():
     print(habit2)
     print(habit3)
 
-    # MySQL logic
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM habits WHERE id = %s AND habit_date = %s", (user_id, today))
-    data = cursor.fetchone()
-    
+    try:
+        with mysql.connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM habits WHERE id = %s AND habit_date = %s", (user_id, today))
+            data = cursor.fetchone()
 
-      #Check if data exist
-    if data:
-        cursor.execute("UPDATE habits SET habit_1 = %s, habit_2 = %s, habit_3 = %s WHERE id = %s AND habit_date = %s", (habit1, habit2, habit3, user_id, today))
-        mysql.connection.commit()
-        cursor.close()
-        # return "data Updated"
-    else:
-        cursor.execute("INSERT INTO habits(habit_1,habit_2,habit_3, habit_1_name, habit_2_name, habit_3_name, id, habit_date) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)", (habit1, habit2, habit3, habit_data[0][6], habit_data[0][7], habit_data[0][8], user_id, today))
-        mysql.connection.commit()
-        cursor.close()
-        # return "data Has been inserted"
+            # Check if data exists
+            if data:
+                cursor.execute("UPDATE habits SET habit_1 = %s, habit_2 = %s, habit_3 = %s WHERE id = %s AND habit_date = %s", (habit1, habit2, habit3, user_id, today))
+                mysql.connection.commit()
+            else:
+                cursor.execute("INSERT INTO habits (habit_1, habit_2, habit_3, habit_1_name, habit_2_name, habit_3_name, id, habit_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (habit1, habit2, habit3, habit_data[0][6], habit_data[0][7], habit_data[0][8], user_id, today))
+                mysql.connection.commit()
 
-    # Redirect user to the dashboard
+    except Exception as e:
+        print(str(e))
+        mysql.connection.rollback()
+
     return redirect(url_for('dashboard'))  # Assuming your dashboard function is named 'dashboard'
 
 
@@ -188,77 +188,88 @@ def machine_learning_integration(id):
     cursor = mysql.connection.cursor()
     user_id = id
    #print(user_id)
-    cursor.execute("SELECT * FROM habits WHERE id = %s", (user_id,))
-    habit_data = cursor.fetchall()
-    #print(habit_data)
+    cursor.execute("SELECT COUNT(*) FROM habits WHERE id = %s", (user_id,))
+    count = cursor.fetchone()
+    print('Line 193',count)
+    if count[0] >= 7:
+        cursor.execute("SELECT * FROM habits WHERE id = %s", (user_id,))
+        habit_data = cursor.fetchall()
+        #print(habit_data)
 
 
-    data_list = list(habit_data)
-    column1 = []
-    column2 = []
-    column3 = []
-    column_date = []
-    is_completed = []
-    for data in data_list:
-        column1.append(data[1])
-        column2.append(data[2])
-        column3.append(data[3])
-        column_date.append(data[4])
-        if data[1] == 1 and data[2] == 1 and data[3] == 1:
-            is_completed.append(1)
-        else:
-            is_completed.append(0)
+        data_list = list(habit_data)
+        column1 = []
+        column2 = []
+        column3 = []
+        column_date = []
+        is_completed = []
+        for data in data_list:
+            column1.append(data[1])
+            column2.append(data[2])
+            column3.append(data[3])
+            column_date.append(data[4])
+            if data[1] == 1 and data[2] == 1 and data[3] == 1:
+                is_completed.append(1)
+            else:
+                is_completed.append(0)
 
-    # Construct the dataframe
-    data = {
-        'Year': [d.year for d in column_date],
-        'Month': [d.month for d in column_date],
-        'Day': [d.day for d in column_date],
-        'DayOfWeek': [d.weekday() for d in column_date],
-        'Habit_1': column1,
-        'Habit_2': column2,
-        'Habit_3': column3,
-        'Is_completed': is_completed
-    }
+        # Construct the dataframe
+        data = {
+            'Year': [d.year for d in column_date],
+            'Month': [d.month for d in column_date],
+            'Day': [d.day for d in column_date],
+            'DayOfWeek': [d.weekday() for d in column_date],
+            'Habit_1': column1,
+            'Habit_2': column2,
+            'Habit_3': column3,
+            'Is_completed': is_completed
+        }
+        
+        df = pd.DataFrame(data)
+
+        # Split the data into training and testing sets
+        X = df.drop('Is_completed', axis=1)
+        y = df['Is_completed']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     
-    df = pd.DataFrame(data)
+        # Train the classifier
+        classifier = RandomForestClassifier(n_estimators=100, random_state=0)
+        classifier.fit(X_train, y_train)
 
-    # Split the data into training and testing sets
-    X = df.drop('Is_completed', axis=1)
-    y = df['Is_completed']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
- 
-    # Train the classifier
-    classifier = RandomForestClassifier(n_estimators=100, random_state=0)
-    classifier.fit(X_train, y_train)
+        # Predict on the test set
+        y_pred = classifier.predict(X_test)
 
-    # Predict on the test set
-    y_pred = classifier.predict(X_test)
-
-    # Predict for the next day
-    today = date.today()
-    next_day = {
-        'Year': [today.year],
-        'Month': [today.month],
-        'Day': [today.day + 1],
-        'DayOfWeek': [(today.weekday() + 1) % 7],
-        'Habit_1': [0],  # Assumed values, replace as needed
-        'Habit_2': [0],  # Assumed values, replace as needed
-        'Habit_3': [0]   # Assumed values, replace as needed
-    }
-    next_day_df = pd.DataFrame(next_day)
-    prediction_for_next_day = classifier.predict(next_day_df)
-    print(next_day_df)
-    print(prediction_for_next_day)
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f"Model Accuracy: {accuracy * 100:.2f}%")
-    return prediction_for_next_day
+        # Predict for the next day
+        today = date.today()
+        next_day = {
+            'Year': [today.year],
+            'Month': [today.month],
+            'Day': [today.day + 1],
+            'DayOfWeek': [(today.weekday() + 1) % 7],
+            'Habit_1': [0],  # Assumed values, replace as needed
+            'Habit_2': [0],  # Assumed values, replace as needed
+            'Habit_3': [0]   # Assumed values, replace as needed
+        }
+        next_day_df = pd.DataFrame(next_day)
+        prediction_for_next_day = classifier.predict(next_day_df)
+        print(next_day_df)
+        print(prediction_for_next_day)
+        accuracy = accuracy_score(y_test, y_pred)
+        #print(f"Model Accuracy: {accuracy * 100:.2f}%")
+        return prediction_for_next_day
+    else:
+        return False
 
 def display_result(prediction):
-    if prediction[0] == 1:
-        return "You will complete your goal"
+    if prediction != False:
+        if prediction[0] == 1:
+            return "You will complete your goal"
+        
+        else:
+            return "You wont complete your goal"
     else:
-        return "You wont complete your goal"
+        return "Not enough data"
+
 # Run the app
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT',5000)))
+    app.run(debug=False,host="0.0.0.0", port=int(os.environ.get('PORT',3000)))
